939e930e402893615e3d0c36726250319a3a8ba4
Your Name <nktclt@gmail.com>
2018-10-12 11:55:34 +0530

    resize function updated

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ import os
 TARGER_DIR = '/home/navaneeth/work/oneon/detected'
 epoches = 3
 image = cv2.imread("/home/navaneeth/work/oneon/test.jpg")
-(h, w) = image.shape[:2]
-center = (w / 2, h / 2)
+h1 = 100
+w1 = 100
 
 
 def rotate(image):
+    (h, w) = image.shape[:2]
+    center = (w / 2, h / 2)
     M = cv2.getRotationMatrix2D(center, 180, 1.0)
     rotated = cv2.warpAffine(image, M, (w, h))
     rotate_imgs = os.path.join(TARGER_DIR, "rota_" + str(i) + ".png")
@@ -17,8 +19,8 @@ def rotate(image):
 
 
 def resize(image):
-    r = h/image.shape[1]
-    dim = (w, int(image.shape[0]*r))
+    r = h1/image.shape[1]
+    dim = (w1, int(image.shape[0]*r))
     resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
     resiz_imgs = os.path.join(TARGER_DIR, "resiz_" + str(i) + ".png")
     cv2.imwrite(resiz_imgs, resized)
@@ -27,4 +29,6 @@ def resize(image):
 for i in range(epoches):
     rotate(image)
     resize(image)
+    h1 = h1*2
+    w1 = w1*2
     i = i+1
