10a9297f066b0d92bd2dc6e6f0a1d50e2c5120d6
Suparna <suparna@qburst.com>
2018-10-27 11:55:48 +0530

    Remove bounding box from images.

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import cv2
 import os
-import PIL
 from PIL import Image
 import numpy as np
-TARGER_DIR = '/home/navaneeth/work/oneon/detected'
+
+
+TARGER_DIR = './detected'
 basewidth = 600
-image0 = Image.open('/home/navaneeth/work/oneon/test.jpg')
+image0 = Image.open('./test.jpg')
 wpercent = (basewidth / float(image0.size[0]))
 hsize = int((float(image0.size[1]) * float(wpercent)))
-image0 = image0.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
+image0 = image0.resize((basewidth, hsize), Image.ANTIALIAS)
 image0.save('resized_image.jpg')
-image = cv2.imread("/home/navaneeth/work/oneon/resized_image.jpg")
+image = cv2.imread("./resized_image.jpg")
 epoches = 1
 h1 = 100
 w1 = 100
@@ -51,7 +52,7 @@ def image_b(image):
         cv2.imwrite(mblur_imgs, median)
         cv2.imwrite(bblur_imgs, bilateral)
         cv2.imwrite(gblur_imgs, gaussian)
-        return 0
+    return 0
 
 
 def image_grey(image):
@@ -59,21 +60,18 @@ def image_grey(image):
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         resiz_imgs = os.path.join(TARGER_DIR, "grey_" + str(i) + ".png")
         cv2.imwrite(resiz_imgs, gray_image)
-        return 0
+    return 0
 
 
-def face_d(image):
+def detect_face(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for i in range(epoches):
         for (x, y, w, h) in faces:
-            img = cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
-            # roi_gray = gray[y:y+h, x:x+w]
-            roi_color = img[y:y+h, x:x+w]
+            roi_color = image[y:y+h, x:x+w]
             face_ = os.path.join(TARGER_DIR, "face_" + str(i) + ".png")
         cv2.imwrite(face_, roi_color)
-        print(faces)
-        return 0
+    return 0
 
 
 def detect_box(image, cropIt=True):
@@ -82,7 +80,8 @@ def detect_box(image, cropIt=True):
     image_y[:, :] = image_yuv[:, :, 0]
     image_blurred = cv2.GaussianBlur(image_y, (3, 3), 0)
     edges = cv2.Canny(image_blurred, 100, 300, apertureSize=3)
-    _, contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    _, contours, _ = cv2.findContours(
+        edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     imo_ = os.path.join(TARGER_DIR, "cont_" + str(i) + ".png")
     cv2.imwrite(imo_, edges)
     return 0
@@ -91,25 +90,26 @@ def detect_box(image, cropIt=True):
 def cont_(image):
     imgray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(imgray, 127, 255, 0)
-    _,  contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    _, contours, _ = cv2.findContours(
+        thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     imo = cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
     imo_ = os.path.join(TARGER_DIR, "edge_" + str(i) + ".png")
     cv2.imwrite(imo_, imo)
     return 0
 
 
-
 def paste_image(image):
     for i in range(epoches):
         image = Image.fromarray(image)  # converting numpy array into PIL image
-        im2 = Image.open('/home/navaneeth/work/oneon/1.png')
+        im2 = Image.open('./1.png')
         x, y = im2.size
         image.paste(im2, (0, 0, x, y))
-        image.save("/home/navaneeth/work/oneon/detected/test_"+str(i)+".jpg", "JPEG")
+        image.save("./detected/test_"+str(i)+".jpg", "JPEG")
+    return 0
 
 
 for i in range(epoches):
-    face_d(image)
+    detect_face(image)
     rotate(image)
     resize(image)
     image_b(image)
@@ -122,5 +122,5 @@ for i in range(epoches):
     w1 = w1*2
     i = i+1
 
-os.system("rm /home/navaneeth/work/oneon/resized_image.jpg")
+os.system("rm ./resized_image.jpg")
 # delete the resized image
