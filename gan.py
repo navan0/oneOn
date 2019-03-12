efc7040f47d13f5994297472628835996b21fa8c
Your Name <nktclt@gmail.com>
2019-03-13 03:03:13 +0530

    update

--- gan.py
+++ gan.py
@@ -5,7 +5,7 @@ epoches = 2
 def paste_image(image):
     for i in range(epoches):
         imageM = Image.fromarray(image)  # converting numpy array into PIL
-        im2 = Image.open('./1.png')
+        im2 = Image.open('/home/navaneeth/work/oneon/1.png')
         imageM.paste(im2, box=None)
         imageM.save("./detected/gan_"+str(i)+".png", "PNG")
     return 0
--- main1.py
+++ main1.py
@@ -1,15 +1,9 @@
 import cv2
 import os
 from PIL import Image
-import argparse
 import numpy as np
-from gan import paste_image
-# from gan import blend
-# from gan import faceBlend
-parser = argparse.ArgumentParser()
-parser.add_argument('-e', '--epoches', help='add number as epoches . eg: python -e 5', required=True)
-args = parser.parse_args()
-# print(args.epoches)
+
+
 TARGER_DIR = '/home/navaneeth/work/oneon/detected'
 basewidth = 600
 image0 = Image.open('/home/navaneeth/work/oneon/test.jpg')
@@ -18,7 +12,7 @@ hsize = int((float(image0.size[1]) * float(wpercent)))
 image0 = image0.resize((basewidth, hsize), Image.ANTIALIAS)
 image0.save('resized_image.jpg')
 image = cv2.imread("resized_image.jpg")
-epoches = int(args.epoches)
+epoches = 1
 h1 = 100
 w1 = 100
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -29,7 +23,7 @@ def rotate(image):
     (h, w) = image.shape[:2]
     center = (w / 2, h / 2)
     M = cv2.getRotationMatrix2D(center, 180, 1.0)
-    rotated = cv2.warpAffine(image, M, (w+i, h+i))
+    rotated = cv2.warpAffine(image, M, (w, h))
     rotate_imgs = os.path.join(TARGER_DIR, "rota_" + str(i) + ".png")
     cv2.imwrite(rotate_imgs, rotated)
     return 0
@@ -62,10 +56,11 @@ def image_b(image):
 
 
 def image_grey(image):
-
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    resiz_imgs = os.path.join(TARGER_DIR, "grey_" + str(i) + ".png")
-    cv2.imwrite(resiz_imgs, gray_image)
+    for i in range(epoches):
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        resiz_imgs = os.path.join(TARGER_DIR, "grey_" + str(i) + ".png")
+        cv2.imwrite(resiz_imgs, gray_image)
+    return 0
 
 
 def detect_face(image):
@@ -87,24 +82,45 @@ def detect_box(image, cropIt=True):
     edges = cv2.Canny(image_blurred, 100, 300, apertureSize=3)
     _, contours, _ = cv2.findContours(
         edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    imo_ = os.path.join(TARGER_DIR, "box_" + str(i) + ".png")
+    imo_ = os.path.join(TARGER_DIR, "cont_" + str(i) + ".png")
     cv2.imwrite(imo_, edges)
     return 0
 
 
+def cont_(image):
+    imgray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+    _, contours, _ = cv2.findContours(
+        thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    imo = cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
+    imo_ = os.path.join(TARGER_DIR, "edge_" + str(i) + ".png")
+    cv2.imwrite(imo_, imo)
+    return 0
+
+
+def paste_image(image):
+    for i in range(epoches):
+        image = Image.fromarray(image)  # converting numpy array into PIL image
+        im2 = Image.open('./1.png')
+        x, y = im2.size
+        image.paste(im2, (0, 0, x, y))
+        image.save("./detected/test_"+str(i)+".jpg", "JPEG")
+    return 0
+
+
 for i in range(epoches):
-    #rotate(image)
-    paste_image(image)
     detect_face(image)
+    rotate(image)
     resize(image)
     image_b(image)
     image_grey(image)
+    cont_(image)
+    #paste_image(image)
     detect_box(image)
-    # blend(image)
 
     h1 = h1*2
     w1 = w1*2
     i = i+1
 
-os.system("rm /home/navaneeth/work/oneon/resized_image.jpg")
+os.system("rm ./resized_image.jpg")
 # delete the resized image
